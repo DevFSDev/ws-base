cc514286c67d7bbb78ac92d10f8830734297ba2a
IBLINETUM\adrian.lopezf.ext <adrian.lopezf.ext@inetum.com>
2023-03-08 14:42:31 +0100

    Segunda actualización

--- src/test/get_test.js
+++ src/test/get_test.js
@@ -1,16 +1,17 @@
 module.exports = async function ({ url, query, body, headers }) {
-
     try {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-        let nombre = query["nombre"]; 
-
-        fetch(`https://api.github.com/users/${nombre}`)
-            .then(response => response.json())
-            .then(data => console.log(" Tu usuario de gitHub es: " + nombre + "\n Tu nombre es: " + data.name
-            + "\n Tu url: " + data.html_url + "\n Fecha de creación del repositorio: " + data.created_at))
-            .catch(error => console.error(error));
-
-
-
-    } catch (error) { console.log('MyError:', error) }
-}
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+      let nombre = query["nombre"]; 
+  
+      const response = await fetch(`https://api.github.com/users/${nombre}`);
+      const data = await response.json();
+      const responseString = "Tu usuario de GitHub es: " + nombre + "\n Tu nombre es: " + data.name +
+        "\n Tu url: " + data.html_url + "\n Fecha de creación del repositorio: " + data.created_at;
+        
+      return responseString;
+  
+    } catch (error) { 
+      console.log('MyError:', error);
+      throw error; // Si hay un error, rechazar la promesa
+    }
+  }
